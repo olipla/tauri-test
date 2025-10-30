9fe2a9beb36e83421e5216f27506237bd05528c7
Ollie Platt <ollie.platt@b4tgroup.co.uk>
2025-10-30 10:57:18 +0000

    Add split panes with nuxt theming

---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="w-screen h-screen">
+    <slot />
+  </div>
+</template>
+
+<style>
+
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,23 @@
 <script lang="ts" setup>
-const items = ref(['Backlog', 'Todo', 'In Progress', 'Done'])
-const value = ref('Backlog')
+import { Pane, Splitpanes } from 'splitpanes'
 </script>
 
 <template>
-  <div>
-    <h1>Hello!</h1>
-    <USelectMenu v-model="value" :items="items" />
+  <div class="w-full h-full">
+    <Splitpanes class="w-full h-full">
+      <Pane min-size="20">
+        1
+      </Pane>
+      <Pane>
+        <Splitpanes horizontal>
+          <Pane>2</Pane>
+          <Pane>3</Pane>
+          <Pane>4</Pane>
+        </Splitpanes>
+      </Pane>
+      <Pane>5</Pane>
+    </Splitpanes>
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
