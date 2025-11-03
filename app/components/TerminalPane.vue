49ab92dcdf0f0d19d6dd8530f0ed452836dbecd0
Ollie Platt <oliverplatt123@gmail.com>
2025-11-03 11:34:18 +0000

    Add extra serial device info to UI mock

--- app/components/TerminalPane.vue
+++ app/components/TerminalPane.vue
@@ -4,10 +4,24 @@
 <template>
   <div class="flex flex-col">
     <div class="flex bg-elevated items-center">
-      <div class="grow pl-2 p-1 flex items-center gap-2">
+      <div class="grow pl-2 p-1 flex items-center gap-3">
         <div class="rounded-full bg-red-400 w-3 h-3" />
-        <div class="font-semibold text-sm">
-          COM15 (9600)
+        <div class="flex flex-col">
+          <div class="font-bold text-sm">
+            COM5
+          </div>
+          <div class=" text-sm">
+            9600
+          </div>
+        </div>
+        <div class="border-muted border self-stretch" />
+        <div class="flex flex-col">
+          <div class="text-muted text-sm">
+            USB Serial Port (COM5)
+          </div>
+          <div class="text-muted text-sm">
+            FTDI / FTDXQ43XA
+          </div>
         </div>
       </div>
       <div class="flex gap-1 p-1">
@@ -16,10 +30,14 @@
         <UButton icon="i-lucide-x" variant="ghost" />
       </div>
     </div>
-    <div class="w-full grow bg-elevated">
+    <div class="w-full grow bg-elevated pl-2 pb-2">
       <SerialTerminal />
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.xterm-scroll-area {
+  padding-right: calc(var(--spacing) * 2) !important;
+}
+</style>
