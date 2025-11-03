308537356d8b6d6de77785a531c64d3703d20053
Ollie Platt <oliverplatt123@gmail.com>
2025-11-03 11:35:33 +0000

    Test listening to a serial port and showing the output in xterm

--- app/components/SerialTerminal.client.vue
+++ app/components/SerialTerminal.client.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { SerialPort } from 'tauri-plugin-serialplugin-api'
+
 const xterm = useTemplateRef('xterm')
 
 watch(xterm, () => {
@@ -10,9 +12,35 @@ watch(xterm, () => {
   }
 }, { immediate: true })
 
-// function onData(data: Uint8Array) {
-//   xterm.value?.terminal.write(data)
-// }
+if (window.__TAURI__) {
+  const port = new SerialPort({
+    path: 'COM4',
+    baudRate: 9600,
+    timeout: 30,
+    size: 1024,
+  })
+  await port.open()
+
+  // Write data
+  await port.write('Hello, Serial Port!')
+
+  // Start port listening
+  await port.startListening()
+
+  // Start port listening
+  const unsubscribe = await port.listen((data) => {
+    console.log('Received:', data)
+    if (data instanceof Uint8Array) {
+      xterm.value?.terminal.write(data)
+    }
+  }, false)
+}
+
+// Stop listening when done
+// await port.cancelListen()
+
+// // Close port
+// await port.close()
 </script>
 
 <template>
