618833f6b9f2457dc0e665f1ab9f9c278c748733
Ollie Platt <oliverplatt123@gmail.com>
2025-11-29 09:34:40 +0000

    Switch persisted storage to local storage

--- app/stores/Configurator.ts
+++ app/stores/Configurator.ts
@@ -116,5 +116,6 @@ export const useConfiguratorStore = defineStore('configurator', () => {
 }, {
   persist: {
     pick: ['serialPortOptions', 'printerConfiguredName'],
+    storage: piniaPluginPersistedstate.localStorage(),
   },
 })
