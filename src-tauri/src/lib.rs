717f2c3066d2541fa6fe28a80561662da69ea76f
Ollie Platt <oliverplatt123@gmail.com>
2026-01-08 14:03:24 +0000

    Refactor lib file to use anyhow

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,57 +1,93 @@
+use anyhow::{Context, Result};
 use tauri::{async_runtime::Mutex, window::Color, Manager};
 
 mod flasher;
 mod printer;
 
+/// Global Tauri application state
+#[derive(Default)]
 pub struct AppData {
+    /// Flag to prevent multiple flashers running at the same time
     bsl_flasher_running: bool,
+}
+
+impl AppData {
+    fn new() -> Self {
+        Self {
+            bsl_flasher_running: false,
+        }
+    }
+}
+
+/// Configure appearance and title of main window
+fn configure_window(window: &tauri::WebviewWindow, version: &str) -> Result<()> {
+    window
+        .set_title(&format!("Jellyfish Configurator {version}"))
+        .context("Failed to set window title")?;
+
+    // Set background colour to match Nuxt theme to avoid flash when starting
+    if let Ok(theme) = window.theme() {
+        let background_colour = match theme {
+            tauri::Theme::Light => Color(255, 255, 255, 255),
+            _ => Color(28, 25, 25, 255),
+        };
+
+        window
+            .set_background_color(Some(background_colour))
+            .context("Failed to set window background colour")
+            .context(theme)?;
+    }
+
+    // The main window is hidden by default, show it now dynamic configuration has been applied
+    window.show().context("Failed to show window")?;
+
+    Ok(())
+}
+
+fn setup_logging(app: &tauri::App) -> Result<()> {
+    if cfg!(debug_assertions) {
+        app.handle()
+            .plugin(
+                tauri_plugin_log::Builder::default()
+                    .level(log::LevelFilter::Info)
+                    // Stop console spam when polling printer status
+                    .level_for("printer_event_handler", log::LevelFilter::Warn)
+                    .build(),
+            )
+            .context("Failed to setup Tauri logging plugin")?;
+    }
+
+    Ok(())
+}
+
+fn setup_app(app: &mut tauri::App) -> Result<()> {
+    app.manage(Mutex::new(AppData::new()));
+
+    setup_logging(app)?;
+
+    let window = app
+        .get_webview_window("main")
+        .context("Could not find main window")?;
+
+    let version = app.package_info().version.to_string();
+
+    configure_window(&window, &version)?;
+
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_serialplugin::init())
         .invoke_handler(tauri::generate_handler![
             printer::get_printers,
             printer::print_data,
             printer::get_printer,
             flasher::flash,
         ])
-        .plugin(tauri_plugin_serialplugin::init())
-        .setup(|app| {
-            app.manage(Mutex::new(AppData {
-                bsl_flasher_running: false,
-            }));
-
-            if cfg!(debug_assertions) {
-                app.handle().plugin(
-                    tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
-                        .level_for("printer_event_handler", log::LevelFilter::Warn)
-                        .build(),
-                )?;
-            }
-
-            let window = app.get_webview_window("main").unwrap();
-            let version = app.package_info().version.to_string();
-
-            let _ = window.set_title(&format!("Jellyfish Configurator {version}"));
-            if let Ok(theme) = window.theme() {
-                match theme {
-                    tauri::Theme::Dark => {
-                        window.set_background_color(Some(Color(28, 25, 25, 255)))?;
-                    }
-                    tauri::Theme::Light => {
-                        window.set_background_color(Some(Color(255, 255, 255, 255)))?;
-                    }
-                    _ => {}
-                }
-            }
-
-            window.show().unwrap();
-
-            Ok(())
-        })
+        .setup(|app| setup_app(app).map_err(Into::into))
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("Error while running Tauri application!");
 }
